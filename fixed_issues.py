feeaf22d00f5093f40f6517099968e7aa8bce14f
Reuben James <20reuben.james@utcportsmouth.org>
2023-10-19 12:53:14 +0100

    Fix issue with files not being found

--- fixed_issues.py
+++ fixed_issues.py
@@ -12,13 +12,16 @@ def Main():
     Score = 0
     while Again == "y":
         Filename = input("Press Enter to start a standard puzzle or enter name of file to load: ")
-        if len(Filename) > 0:
-            MyPuzzle = Puzzle(Filename + ".txt")
-        else:
-            MyPuzzle = Puzzle(8, int(8 * 8 * 0.6))
-        Score = MyPuzzle.AttemptPuzzle()
-        print("Puzzle finished. Your score was: " + str(Score))
-        Again = input("Do another puzzle? ").lower()
+        try:
+            if len(Filename) > 0:
+                MyPuzzle = Puzzle(Filename + ".txt")
+            else:
+                MyPuzzle = Puzzle(8, int(8 * 8 * 0.6))
+            Score = MyPuzzle.AttemptPuzzle()
+            print("Puzzle finished. Your score was: " + str(Score))
+            Again = input("Do another puzzle? ").lower()
+        except FileNotFoundError:
+            print("Puzzle not loaded")
 
 
 class Puzzle():
@@ -55,32 +58,29 @@ class Puzzle():
             self.__AllowedSymbols.append("T")
 
     def __LoadPuzzle(self, Filename):
-        try:
-            with open(Filename) as f:
-                NoOfSymbols = int(f.readline().rstrip())
-                for Count in range(1, NoOfSymbols + 1):
-                    self.__AllowedSymbols.append(f.readline().rstrip())
-                NoOfPatterns = int(f.readline().rstrip())
-                for Count in range(1, NoOfPatterns + 1):
-                    Items = f.readline().rstrip().split(",")
-                    P = Pattern(Items[0], Items[1])
-                    self.__AllowedPatterns.append(P)
-                self.__GridSize = int(f.readline().rstrip())
-                for Count in range(1, self.__GridSize * self.__GridSize + 1):
-                    Items = f.readline().rstrip().split(",")
-                    if Items[0] == "@":
-                        C = BlockedCell()
-                        self.__Grid.append(C)
-                    else:
-                        C = Cell()
-                        C.ChangeSymbolInCell(Items[0])
-                        for CurrentSymbol in range(1, len(Items)):
-                            C.AddToNotAllowedSymbols(Items[CurrentSymbol])
-                        self.__Grid.append(C)
-                self.__Score = int(f.readline().rstrip())
-                self.__SymbolsLeft = int(f.readline().rstrip())
-        except:
-            print("Puzzle not loaded")
+        with open(Filename) as f:
+            NoOfSymbols = int(f.readline().rstrip())
+            for Count in range(1, NoOfSymbols + 1):
+                self.__AllowedSymbols.append(f.readline().rstrip())
+            NoOfPatterns = int(f.readline().rstrip())
+            for Count in range(1, NoOfPatterns + 1):
+                Items = f.readline().rstrip().split(",")
+                P = Pattern(Items[0], Items[1])
+                self.__AllowedPatterns.append(P)
+            self.__GridSize = int(f.readline().rstrip())
+            for Count in range(1, self.__GridSize * self.__GridSize + 1):
+                Items = f.readline().rstrip().split(",")
+                if Items[0] == "@":
+                    C = BlockedCell()
+                    self.__Grid.append(C)
+                else:
+                    C = Cell()
+                    C.ChangeSymbolInCell(Items[0])
+                    for CurrentSymbol in range(1, len(Items)):
+                        C.AddToNotAllowedSymbols(Items[CurrentSymbol])
+                    self.__Grid.append(C)
+            self.__Score = int(f.readline().rstrip())
+            self.__SymbolsLeft = int(f.readline().rstrip())
 
     def AttemptPuzzle(self):
         Finished = False
